52ca7b29d63a38ec6f007a150f2eae854aeeef6a
Mohamad <moeman.bdeir09@gmail.com>
2025-12-22 20:30:42 -0500

    index.js is now entry point

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+#!/usr/bin/env node
+/*
+node index.js <function> <arguments...>
+*/
+
 import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
+import { createUser } from "./createUser.js";
+import { createWebsite } from "./createWebsite.js";
 
 dotenv.config();
 
@@ -13,8 +20,33 @@ const client = new MongoClient(uri, {
   },
 });
 
-export async function getDB() {
+async function main() {
   await client.connect();
   let db = client.db("analytics");
+  const [command, ...args] = process.argv.slice(2);
+  console.log("working...");
+  if (command === "create-user")
+    createUser(db, args[0], args[1])
+      .then(() => {
+        process.exit(0);
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+        process.exit(1);
+      });
+  else if (command === "create-website")
+    createWebsite(db, args[0])
+      .then(() => {
+        process.exit(0);
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+        process.exit(1);
+      });
+  else {
+    console.log("Type it correctly");
+  }
   return db;
 }
+
+main();
