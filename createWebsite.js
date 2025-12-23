b1d4c59d7bdd72f7dfbe9123d499679ce1d075f7
Mohamad <moeman.bdeir09@gmail.com>
2025-12-22 20:29:52 -0500

    add website from cli

--- createWebsite.js
+++ createWebsite.js
@@ -1,32 +1,17 @@
-/*
-How to start: node createUser.js email password
-*/
-import getDB from "./index.js";
 import crypto from "crypto";
 
-async function main() {
-  const db = await getDB();
+export async function createWebsite(db, websiteName) {
   const users = db.collection("websites");
 
-  const websiteName = process.argv.slice(2);
-
   if (!websiteName) {
-    console.log("Use: node createWebsite.js <website name> <email>");
+    console.log("Use: analytics-cli create-website <website name>");
     process.exit(1);
   }
 
   await users.insertOne({
     _id: "site_" + crypto.randomBytes(8).toString("hex"),
+    // add owner id from appdata to owners.
     owners: [],
     createdAt: new Date(),
   });
 }
-
-main()
-  .then(() => {
-    process.exit(0);
-  })
-  .catch((err) => {
-    console.log(`Error: ${err}`);
-    process.exit(1);
-  });
