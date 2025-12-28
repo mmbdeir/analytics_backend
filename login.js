583e784662a71eddd923cb86c8ae3c5bd099a106
Mohamad <moeman.bdeir09@gmail.com>
2025-12-28 14:50:33 -0500

    Login throws error and is handled in index.js

--- login.js
+++ login.js
@@ -2,23 +2,22 @@ import bcrypt from "bcrypt";
 
 export async function login(db, email, password) {
   const users = db.collection("users");
-  try {
-    const userDocument = await users.findOne({
-      _id: email,
-    });
-    if (!userDocument) {
-      console.log("Couldn't find email.");
-      return false;
-    }
-    const compare = await bcrypt.compare(password, userDocument.passwordHash);
-    if (!compare) {
-      console.log("Make sure the email and password are correct.");
-      return false;
-    }
-    //Login logic
-    // console.log("Login successful");
-  } catch (e) {
-    console.log("Login failed due to error:", e);
-    return false;
+  if (!email || !password) {
+    throw new Error("Use: analytics-cli create-user <email> <password>");
   }
+
+  const userDocument = await users.findOne({ _id: email });
+
+  if (!userDocument) {
+    throw new Error("Couldn't find email.");
+  }
+
+  const match = await bcrypt.compare(password, userDocument.passwordHash);
+
+  if (!match) {
+    throw new Error("Make sure the email and password are correct.");
+  }
+
+  //Login logic
+  // console.log("Login successful");
 }
