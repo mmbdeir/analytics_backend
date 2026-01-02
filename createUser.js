9ae0103be98c8c9f05fff0f55952344ab2e00efa
Mohamad <moeman.bdeir09@gmail.com>
2026-01-02 01:36:58 -0500

    Made it path

--- createUser.js
+++ createUser.js
@@ -1,19 +1,23 @@
 import bcrypt from "bcrypt";
+import express from "express";
 
-export async function createUser(db, email, password) {
+export function createUser(db) {
+  const router = express.Router();
   const users = db.collection("users");
 
-  if (!email || !password) {
-    throw new Error("Use: analytics-cli create-user <email> <password>.");
-  }
+  router.post("/:email/:password", async (req, res) => {
+    const email = req.params.email;
+    const password = req.params.password;
+    const passwordHash = await bcrypt.hash(password, 10);
 
-  const passwordHash = await bcrypt.hash(password, 10);
-
-  // This will throw an error automatically
-  await users.insertOne({
-    _id: email,
-    passwordHash,
-    createdAt: new Date(),
-    role: "user",
+    // This will throw an error automatically
+    await users.insertOne({
+      _id: email,
+      passwordHash,
+      createdAt: new Date(),
+      role: "user",
+    });
+    res.json({ statusCode: 200 });
   });
+  return router;
 }
