27cb7deb7e755af78db475654a7301dfca158b1a
Mohamad <moeman.bdeir09@gmail.com>
2026-01-02 01:17:22 -0500

    Handles website creation request to post in mongodb

--- createWebsite.js
+++ createWebsite.js
@@ -1,16 +1,28 @@
 import crypto from "crypto";
+import express from "express";
 
-export async function createWebsite(db, websiteName) {
-  const websites = db.collection("websites");
+export function createWebsiteRouter(db) {
+  const router = express.Router();
 
-  if (!websiteName) {
-    throw new Error("Use: analytics-cli create-website <website name>.");
-  }
+  router.post("/:websiteName", async (req, res) => {
+    const websites = db.collection("websites");
+    const websiteName = req.params.websiteName;
 
-  await websites.insertOne({
-    _id: "site_" + crypto.randomBytes(8).toString("hex"),
-    // add owner id from appdata to owners.
-    owners: [],
-    createdAt: new Date(),
+    if (!websiteName) {
+      return res
+        .status(400)
+        .json({ error: "Use: analytics-cli create-website <website name>." });
+    }
+
+    // This will throw an error automatically cuz mongodb handles that
+    await websites.insertOne({
+      _id: "site_" + crypto.randomBytes(8).toString("hex"),
+      name: websiteName,
+      // add owner id from appdata to owners.
+      owners: [],
+      createdAt: new Date(),
+    });
+    res.json({ success: true });
   });
+  return router;
 }
