b1fd41358b279846161e731b2019b0ecb1b7528a
Mohamad <moeman.bdeir09@gmail.com>
2026-01-05 16:08:27 -0500

    Added error hanlding that returns http res

--- createUser.js
+++ createUser.js
@@ -6,18 +6,28 @@ export function createUser(db) {
   const users = db.collection("users");
 
   router.post("/:email/:password", async (req, res) => {
-    const email = req.params.email;
-    const password = req.params.password;
-    const passwordHash = await bcrypt.hash(password, 10);
+    try {
+      const email = req.params.email;
+      const password = req.params.password;
+      const passwordHash = await bcrypt.hash(password, 10);
 
-    // This will throw an error automatically
-    await users.insertOne({
-      _id: email,
-      passwordHash,
-      createdAt: new Date(),
-      role: "user",
-    });
-    res.json({ statusCode: 200 });
+      // This will throw an error automatically
+      await users.insertOne({
+        _id: email,
+        passwordHash,
+        createdAt: new Date(),
+        role: "user",
+      });
+      res.status(200).json({ success: "True" });
+    } catch (err) {
+      if (err.code == 11000) {
+        return res
+          .status(409)
+          .json({ error: "User with email already exists." });
+      }
+      return res.status(500).json({ error: err });
+    }
+    //HANDLE DUPLICATE KEY ERROR
   });
   return router;
 }
