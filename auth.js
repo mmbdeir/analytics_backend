8facffbef4dfe3e38c697e53c4f5c6bd469463fb
Mohamad <moeman.bdeir09@gmail.com>
2026-01-07 21:59:34 -0500

    Correctly using bearer

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ export function auth(req, res, next) {
       error: "No token recieved in headers.",
     });
 
-  const parts = headers.split(" ");
+  const parts = bearer.split(" ");
   if (parts.length !== 2 || parts[0] !== "Bearer") {
     return res.status(401).json({ error: "Invalid Authorization header" });
   }
