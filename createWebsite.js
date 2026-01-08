7a7bd30e4d6cf0b0bd39eb4b5d92210190c24b79
Mohamad <moeman.bdeir09@gmail.com>
2026-01-07 20:13:20 -0500

    Uses jwt token to from where it came from

--- createWebsite.js
+++ createWebsite.js
@@ -1,19 +1,21 @@
 import crypto from "crypto";
 import express from "express";
+import auth from "./auth.js";
 
 export function createWebsiteRouter(db) {
   const router = express.Router();
   const websites = db.collection("websites");
 
-  router.post("/:websiteName", async (req, res) => {
+  router.post("/:websiteName", auth, async (req, res) => {
     const websiteName = req.params.websiteName;
+    const creator = req.users.email;
 
     // This will throw an error automatically cuz mongodb handles that
     await websites.insertOne({
       _id: "site_" + crypto.randomBytes(8).toString("hex"),
       name: websiteName,
       // add owner id from appdata to owners.
-      admin: [],
+      admin: [creator],
       createdAt: new Date(),
     });
     res.status(200).json({ success: "True" });
