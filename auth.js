920a256221858aa0d47849a599386bc61be705a6
Mohamad <moeman.bdeir09@gmail.com>
2026-01-07 20:59:36 -0500

    Fix bug

--- auth.js
+++ auth.js
@@ -1,27 +1,20 @@
 import jwt from "jsonwebtoken";
 
 export function auth(req, res, next) {
-  const bearer = req.headers.token;
+  const bearer = req.headers.authorization;
   if (!bearer)
     res.json({
       error: "No token recieved in headers.",
     });
 
-  const parts = header.split(" ");
-  if (parts.length !== 2 || parts[0] !== "Bearer") {
-    return res
-      .status(401)
-      .json({ error: "Invalid Authorization header format." });
-  }
-
-  const token = parts[1];
+  const token = bearer.split(" ")[1];
 
   jwt.verify(token, process.env.SECRET, (err, decoded) => {
     if (err) {
       console.log(err);
       return res.json({ error: "Invalid or expire token" });
     }
-    req.user = decoded;
+    req.email = decoded;
     next();
   });
 }
